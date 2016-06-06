5228f91128d185e7c64777aad6fc915338cc1a2a
Uri Kalish <uri.kalish@hpe.com>
2016-06-06 19:26:23 +0300

    misc

--- game-of-life.js
+++ game-of-life.js
@@ -14,16 +14,127 @@
 	var baseColors = [];
 	var colorsRGB = [ [0x33, 0xFF, 0x99], [0xFF, 0xB7, 0xB7] ]; //green and red
 	var armies = [];
-	
+	var htmlHelper = new HtmlHelper();
+
+
+
+	// HTML Helper -------------------------------------------------------------------------------------------------------
+	function HtmlHelper() {
+
+		var that = this;
+
+		that.getColorHexStr = function getColorHexStr(colorRGBArray) {
+			return colorRGBArray[0].toString(16) + colorRGBArray[1].toString(16) + colorRGBArray[2].toString(16);
+		}
+
+		that.addCssRule = function addCssRule(cssText) {
+			var style = document.createElement('style');
+			style.type = 'text/css';
+			if (style.styleSheet) {
+				style.styleSheet.cssText = cssText;
+			} else {
+				style.appendChild(document.createTextNode(cssText));
+			}
+			document.head.appendChild(style);
+		}
+
+		that.addCssRules = function addCssRules() {
+			that.addCssRule('* {box-sizing: border-box;}');
+			that.addCssRule('html {height: 100%;}');
+			that.addCssRule('body {height: 100%; margin: 0; overflow: hidden; background-color: #222; color: #FFF; font-family: monospace; font-size: 15px;}');
+			that.addCssRule('#gol-container {height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;}');
+			that.addCssRule('#gol-canvas {background-color: #000000;}');
+			that.addCssRule('#gol-army-line-0 {margin: 5px; color: #' + that.getColorHexStr(colorsRGB[0]) + ';}');
+			that.addCssRule('#gol-army-line-1 {margin: 5px; color: #' + that.getColorHexStr(colorsRGB[1]) + ';}');
+		}
+
+		that.addContainer = function addContainer() {
+			var container = document.createElement('div');
+			container.setAttribute('id', 'gol-container');
+			return document.body.appendChild(container);
+			;
+		}
+
+		that.addArmyLine = function addArmyLine(container, army) {
+			var textNode,
+					armyLine = document.createElement('div');
+			armyLine.setAttribute('id', 'gol-army-line-' + army.index);
+			textNode = document.createTextNode(army.name);
+			armyLine.appendChild(textNode);
+			return container.appendChild(armyLine);
+		}
+
+		that.addCanvas = function addCanvas(container) {
+			var canvas = document.createElement('canvas');
+			canvas.setAttribute('id', 'gol-canvas');
+			canvas.setAttribute('width', cols + 'px');
+			canvas.setAttribute('height', rows + 'px');
+			return container.appendChild(canvas);
+		}
+
+		that.drawMatrixToCanvas = function drawMatrixToCanvas(curMatrix, nxtMatrix) {
+			var i, x, y,
+					imgData = ctx.createImageData(cols, rows);
+			for (y = 0; y < rows; y++) {
+				for (x = 0; x < cols; x++) {
+					i = y * cols + x;
+					if (curMatrix[i] !== nxtMatrix[i]) {
+						imgData.data[i * 4] =	colorsRGB[baseColors[i]][0];
+						imgData.data[i * 4 + 1] = colorsRGB[baseColors[i]][1];
+						imgData.data[i * 4 + 2] = colorsRGB[baseColors[i]][2];
+						imgData.data[i * 4 + 3] = Math.trunc(nxtMatrix[i] * 255);
+					}
+				}
+			}
+			ctx.putImageData(imgData, 0, 0);
+		}
+
+	}
+	// HTML Helper -------------------------------------------------------------------------------------------------------
+
+
+
+	// Army --------------------------------------------------------------------------------------------------------------
+	function Army(index, name, color) {
+
+		var that = this;
+
+		that.index = index;
+		that.name = name;
+		that.color = color;
+	}
+	// Army --------------------------------------------------------------------------------------------------------------
+
+
+
+	// Board -------------------------------------------------------------------------------------------------------------
+	function Board(cols, rows) {
+
+		var that = this;
+
+		that.cols = cols;
+		that.rows = rows;
+		that.points = cols * rows;
+	}
+	// Board -------------------------------------------------------------------------------------------------------------
+
+
+
+	function addArmy(name) {
+		var index = armies.length,
+				army = new Army(index, name, colorsRGB[index]);
+		armies.push(army);
+	}
+
 	function initGameOfLife() {
-		var i, r, c, container, canvas;
-		addCssRules();
-		container = addContainer();
+		var i, container, canvas;
+		htmlHelper.addCssRules();
+		container = htmlHelper.addContainer();
 		addArmy('Green Army');
 		addArmy('Red Army');
-		addArmyLine(container, armies[1]);
-		canvas = addCanvas(container);
-		addArmyLine(container, armies[0]);
+		htmlHelper.addArmyLine(container, armies[1]);
+		canvas = htmlHelper.addCanvas(container);
+		htmlHelper.addArmyLine(container, armies[0]);
 		ctx = canvas.getContext('2d');
 		imgData = ctx.createImageData(cols, rows);
 		for (i = 0; i < points; i++) {
@@ -33,90 +144,11 @@
 		setTimeout(onTick, 0);
 	}
 
-	function Army(index, name, color) {
-		this.index = index;
-		this.name = name;
-		this.color = color;
-	}
-
-	function addArmy(name) {
-		var index = armies.length;
-		var army = new Army(index, name, colorsRGB[index]);
-		armies.push(army);
-	}
-
-	function getColorHex(colorRGBArray) {
-		return colorRGBArray[0].toString(16) + colorRGBArray[1].toString(16) + colorRGBArray[2].toString(16);
-	}
-
-	function addCssRule(cssText) {
-		var style = document.createElement('style');
-		style.type = 'text/css';
-		if (style.styleSheet) {
-			style.styleSheet.cssText = cssText;
-		} else {
-			style.appendChild(document.createTextNode(cssText));
-		}
-		document.head.appendChild(style);
-	}
-
-	function addCssRules() {
-		addCssRule('* {box-sizing: border-box;}');
-		addCssRule('html {height: 100%;}');
-		addCssRule('body {height: 100%; margin: 0; overflow: hidden; background-color: #222; color: #FFF; font-family: monospace; font-size: 15px;}');
-		addCssRule('#gol-container {height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;}');
-		addCssRule('#gol-canvas {background-color: #000000;}');
-		addCssRule('#gol-army-line-0 {margin: 5px; color: #' + getColorHex(colorsRGB[0]) + ';}');
-		addCssRule('#gol-army-line-1 {margin: 5px; color: #' + getColorHex(colorsRGB[1]) + ';}');
-	}
-
-	function addContainer() {
-		var container = document.createElement('div');
-		container.setAttribute('id', 'gol-container');
-		return document.body.appendChild(container);
-		;
-	}
-
-	function addArmyLine(container, army) {
-		var armyLine;
-		armyLine = document.createElement('div');
-		armyLine.setAttribute('id', 'gol-army-line-' + army.index);
-		var textNode = document.createTextNode(army.name);
-		armyLine.appendChild(textNode);
-		return container.appendChild(armyLine);
-	}
-
-	function addCanvas(container) {
-		var canvas;
-		canvas = document.createElement('canvas');
-		canvas.setAttribute('id', 'gol-canvas');
-		canvas.setAttribute('width', cols + 'px');
-		canvas.setAttribute('height', rows + 'px');
-		return container.appendChild(canvas);
-	}
-
 //function calcBrightness(hex) {
 //    var rgb=[],r=hex>>16&0xFF;g=hex>>8&0xFF;b=hex&0xFF;
 //    rgb.push(r, g, b);
 //    return Math.sqrt((rgb[= 0] * rgb[0] * 0.241) + (rgb[1] * rgb[1] * 0.691) + (rgb[2] * rgb[2] * 0.68) ) / 255;
 //}
-
-	function drawMatrixToCanvas(curMatrix, nxtMatrix) {
-		var imgData = ctx.createImageData(cols, rows);
-		var i, x, y;
-		for (y = 0; y < rows; y++) {
-			for (x = 0; x < cols; x++) {
-				i = y * cols + x;
-				if (curMatrix[i] !== nxtMatrix[i]) {
-					imgData.data[i * 4] = baseColors[i] === 0 ? colorsRGB[0][0] : colorsRGB[1][0];
-					imgData.data[i * 4 + 1] = baseColors[i] === 0 ? colorsRGB[0][1] : colorsRGB[1][1];
-					imgData.data[i * 4 + 2] = baseColors[i] === 0 ? colorsRGB[0][2] : colorsRGB[1][2];
-					imgData.data[i * 4 + 3] = Math.trunc(nxtMatrix[i] * 255);
-				}
-			}
-		}
-		ctx.putImageData(imgData, 0, 0);
-	}
 
 	function copyMatrixValues(srcMatrix, dstMatrix) {
 		var i;
@@ -238,7 +270,7 @@
 		ageMatrixValues(matrices[0]);
 		addRandomLife(matrices[0]);
 		computeNextState(matrices[0], matrices[1]);
-		drawMatrixToCanvas(matrices[2], matrices[1]);
+		htmlHelper.drawMatrixToCanvas(matrices[2], matrices[1]);
 		copyMatrixValues(matrices[1], matrices[2]);
 		if (ticks % turnsPerGeneration === 0) {
 			endGeneration();
