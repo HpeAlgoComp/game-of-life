ecdfb4ea691386b8121f870bfb8d3f8f9a1db646
Uri Kalish <urikalish@users.noreply.github.com>
2016-07-12 01:00:01 +0300

    colors

--- gol-settings.js
+++ gol-settings.js
@@ -2,7 +2,7 @@ function GolSettings() {
 
 	var that = this;
 
-	that.colorsRGB = [ [0x33, 0xFF, 0x99], [0xFF, 0xB7, 0xB7] ]; //green and red
+	that.colorsRGB = [ [0x11, 0xFF, 0xCC], [0xFF, 0x33, 0x33] ]; //green and red
 	that.cols = 400;
 	that.rows = 200;
 }
