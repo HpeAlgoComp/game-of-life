50d6f61a00a4012121209fcff9aaf50e8c9f4fce
Uri Kalish <urikalish@users.noreply.github.com>
2016-07-15 23:59:19 +0300

    tiny

--- platform/gol-settings.js
+++ platform/gol-settings.js
@@ -2,7 +2,7 @@ function GolSettings() {
 
 	var that = this;
 
-	that.colorsRGB = [ [0x11, 0xFF, 0xCC], [0xFF, 0x33, 0x33] ]; //green and red
+	that.colorsRGB = [ [0x11, 0xFF, 0xCC], [0xFF, 0x33, 0x33] ];
 	that.cols = 400;
 	that.rows = 200;
 	that.budgetTickQuantum = 1;
