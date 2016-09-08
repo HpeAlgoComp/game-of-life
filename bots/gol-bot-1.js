e06f9c1f70329a30c2f8b57ed5c845d126b0a2be
Uri Kalish <uri.kalish@hpe.com>
2016-09-08 18:39:32 +0300

    bot plans

--- bots/gol-bot-1.js
+++ bots/gol-bot-1.js
@@ -1,76 +1,32 @@
 (function() {
 
-	setTimeout(function registerArmy() {
-		window.registerArmy({
-			name: 'BOT_ONE',
-			icon: '',
-			cb: cb
-		});
-	}, 2000);
+	// utilities ---------------------------------------------------------------------------------------------------------
 
-	var plan1 = [
-		'spaceship'
-	];
-	var plan2 = [
-		'fence'
-	];
-	var plan3 = [
-		'spaceship',
-		'glider', 
-		'mine',
-		'fence'
-	];
-	var planIndex = 0;
-  var fenceLocation = 0;
-
-	function cb(data) {
-		var pixels = [];
-		var plan;
-		if (data.generation === 1) {
-			planIndex = 0;
-      fenceLocation = 0;
-		}
-		if (data.generation < 200) {
-			plan = plan1;
-		} else if (data.generation < 440) {
-			plan = plan2;
-		} else {
-			plan = plan3;
-		}
-		if (plan[planIndex] === 'mine') {
-			pixels = tryPlaceMine(data);
-		} else if (plan[planIndex] === 'fence') {
-			pixels = tryPlaceFence(data);
-		} else if (plan[planIndex] === 'glider') {
-			pixels = tryPlaceGlider(data);
-		} else if (plan[planIndex] === 'spaceship') {
-			pixels = tryPlaceSpaceship(data);
-		}
-		if (pixels.length > 0) {
-			planIndex = (planIndex + 1) % plan.length;
-		}
-		return pixels;
+	function getRnd(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
-	function tryPlaceMine(data) {
+	// structures --------------------------------------------------------------------------------------------------------
+
+	function tryPlaceMine(data, col, row) {
 		var pixels = [];
 		var r, c;
 		if (data.budget >= 3) {
-			c = Math.floor(Math.random() * (data.cols - 1));
-			r = Math.floor(Math.random() * 60) + 20;
+			c = col || getRnd(0, data.cols - 2);
+			r = row || getRnd(20, 80);
 			pixels.push([c, r]);
 			pixels.push([c, r+1]);
-			pixels.push([c+1, r]);			
+			pixels.push([c+1, r]);
 		}
 		return pixels;
 	}
 
-	function tryPlaceFence(data) {
+	function tryPlaceFence(data, col, row) {
 		var pixels = [];
 		var r, c;
 		if (data.budget >= 3) {
-			c = fenceLocation;
-			r = data.rows - 15;
+			c = col || fenceLocation;
+			r = row || data.rows - 15;
 			pixels.push([c, r]);
 			pixels.push([c+1, r]);
 			pixels.push([c, r+1]);
@@ -82,27 +38,27 @@
 		return pixels;
 	}
 
-	function tryPlaceGlider(data) {
+	function tryPlaceGlider(data, col, row) {
 		var pixels = [];
 		var r, c;
 		if (data.budget >= 5) {
-			c = Math.floor(Math.random() * (data.cols - 2));
-			r = 0;
+			c = col || getRnd(0, data.cols - 3);
+			r = row || getRnd(0, data.rows - 3);
 			pixels.push([c, r]);
 			pixels.push([c+1, r]);
 			pixels.push([c+2, r]);
-			pixels.push( Math.floor(Math.random()*2) === 0 ? [c, r+1] : [c+2, r+1]);
+			pixels.push(getRnd(0, 1) === 0 ? [c, r+1] : [c+2, r+1]);
 			pixels.push([c+1, r+2]);
 		}
 		return pixels;
 	}
 
-	function tryPlaceSpaceship(data) {
+	function tryPlaceSpaceship(data, col, row) {
 		var pixels = [];
 		var r, c;
 		if (data.budget >= 9) {
-			c = Math.floor(Math.random() * (data.cols - 3));
-			r = 0;
+			c = col || getRnd(0, data.cols - 4);
+			r = row || 0;
 			if (c < data.cols / 2) {
 				pixels.push([c+1, r]);
 				pixels.push([c+2, r]);
@@ -122,10 +78,77 @@
 				pixels.push([c, r+2]);
 				pixels.push([c, r+3]);
 				pixels.push([c+1, r+4]);
-				pixels.push([c+3, r+4]);				
+				pixels.push([c+3, r+4]);
 			}
 		}
 		return pixels;
 	}
+
+	// bots --------------------------------------------------------------------------------------------------------------
+
+	var bot1 = function bot1(data) {
+		var pixels = [];
+		var plan;
+		if (data.generation === 1) {
+			planIndex = 0;
+      fenceLocation = 0;
+		}
+		if (data.generation < 200) {
+			plan = ['spaceship'];
+		} else if (data.generation < 440) {
+			plan = ['fence'];
+		} else {
+			plan = ['spaceship', 'glider', 'mine', 'fence'];
+		}
+		if (plan[planIndex] === 'mine') {
+			pixels = tryPlaceMine(data);
+		} else if (plan[planIndex] === 'fence') {
+			pixels = tryPlaceFence(data);
+		} else if (plan[planIndex] === 'glider') {
+			pixels = tryPlaceGlider(data, null, 0);
+		} else if (plan[planIndex] === 'spaceship') {
+			pixels = tryPlaceSpaceship(data, null, 0);
+		}
+		if (pixels.length > 0) {
+			planIndex = (planIndex + 1) % plan.length;
+		}
+		return pixels;
+	};
+
+	var bot2 = function bot2(data) {
+		var pixels = [];
+		var plan;
+		if (data.generation === 1) {
+			planIndex = 0;
+			fenceLocation = 0;
+		}
+		plan = ['glider', 'spaceship'];
+		if (plan[planIndex] === 'glider') {
+			pixels = tryPlaceGlider(data);
+		} else if (plan[planIndex] === 'spaceship') {
+			pixels = tryPlaceSpaceship(data, null, 0);
+		}
+		if (pixels.length > 0) {
+			planIndex = (planIndex + 1) % plan.length;
+		}
+		return pixels;
+	};
+
+	// init --------------------------------------------------------------------------------------------------------------
+
+	var planIndex = 0;
+	var fenceLocation = 0;
+	var bots = [
+		{name: 'BOT_1', icon:'bot', cb: bot1},
+		{name: 'BOT_2', icon:'bot', cb: bot2}
+	];
+	var bot = bots[getRnd(0, bots.length-1)];
+	setTimeout(function registerArmy() {
+		window.registerArmy({
+			name: bot.name,
+			icon: bot.icon,
+			cb: bot.cb
+		});
+	}, 2000);
 
 })();
